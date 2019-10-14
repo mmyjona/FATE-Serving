9422ab5b4a18f105ab5afad093239f553fcf3f89
v_dylanxu <136539068@qq.com>
2019-10-14 10:30:39 +0800

    add jmx
    Signed-off-by: v_dylanxu <136539068@qq.com>

--- serving-server/src/main/java/com/webank/ai/fate/serving/ServingServer.java
+++ serving-server/src/main/java/com/webank/ai/fate/serving/ServingServer.java
@@ -150,8 +150,9 @@ public class ServingServer implements InitializingBean {
             boolean useJMX = Boolean.valueOf(Configuration.getProperty(Dict.USE_JMX));
             if (useJMX) {
                 String jmxServerName = Configuration.getProperty(Dict.JMX_SERVER_NAME, "serving");
+                int jmxPort = Integer.valueOf(Configuration.getProperty(Dict.JMX_PORT, "9999"));
                 FateMBeanServer fateMBeanServer = new FateMBeanServer(ManagementFactory.getPlatformMBeanServer(), true);
-                String jmxServerUrl = fateMBeanServer.openJMXServer(jmxServerName);
+                String jmxServerUrl = fateMBeanServer.openJMXServer(jmxServerName, jmxPort);
                 // service:jmx:rmi:///jndi/rmi://127.0.0.1:9999/serving
                 // /FATE-SERVICES/jmx/providers/service:jmx:rmi:///jndi/rmi://10.56.224.80:9999/serving
                 URL jmxUrl = URL.parseJMXServiceUrl(jmxServerUrl);
